41852f0d23b86a9fba3373a011b8ec68de965f09
Kennedy-Mwaura <kennedy.mwaura@student.moringa.school.com>
2021-07-01 13:42:28 +0300

    imported africas talking python sdk

--- airtime_disbursal.py
+++ airtime_disbursal.py
@@ -1,8 +1,11 @@
+import os
+import africastalking as at
 import gspread
 
-gc = gspread.service_account(filename='airtime-credentials.json')
+# Get your service account credentials file from https://console.cloud.google.com/
+gc = gspread.service_account(filename='airtime-credentials.json')  # Change the name as appropriate
 sh = gc.open('Contact Information (Responses)')
-print(sh.sheet1.get('B2'))
+# print(sh.sheet1.get('B2'))
 
 
 def get_spreadsheet_data(sheet_name, worksheet_index):
@@ -14,3 +17,5 @@ def get_spreadsheet_data(sheet_name, worksheet_index):
 sheet_index = 0
 g = get_spreadsheet_data('Contact Information (Responses)', sheet_index)
 print(g)
+for x in g:
+    print(x[4])
